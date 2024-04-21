cf9ef2f18187cb270e31d48b3835403168b2779a
Chris Holden <ceholden@gmail.com>
2024-04-21 19:44:02 -0400

    add map

---
+++ crabioware/crates/games/paccrab/assets/tilemap.tsx
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tileset" tilewidth="8" tileheight="8" tilecount="43" columns="42">
+ <editorsettings>
+  <export target="tilemap.json" format="json"/>
+ </editorsettings>
+ <transformations hflip="0" vflip="0" rotate="0" preferuntransformed="1"/>
+ <image source="tiles.png" width="336" height="8"/>
+ <tile id="2" type="Wall"/>
+ <tile id="5" type="Wall"/>
+ <tile id="1" type="Wall"/>
+ <tile id="4" type="Wall"/>
+ <tile id="0" type="Wall"/>
+ <tile id="15" type="Wall"/>
+ <tile id="18" type="Wall"/>
+ <tile id="6" type="Wall"/>
+ <tile id="8" type="Wall"/>
+ <tile id="11" type="Wall"/>
+ <tile id="9" type="Wall"/>
+ <tile id="10" type="Wall"/>
+ <tile id="12" type="Wall"/>
+ <tile id="13" type="Wall"/>
+ <tile id="14" type="Wall"/>
+ <tile id="17" type="Wall"/>
+ <tile id="16" type="Wall"/>
+ <tile id="19" type="Wall"/>
+ <tile id="20" type="Wall"/>
+ <tile id="21" type="Wall"/>
+ <tile id="25" type="Wall"/>
+ <tile id="22" type="Wall"/>
+ <tile id="23" type="Wall"/>
+ <tile id="29" type="Wall"/>
+ <tile id="38" type="Enemy">
+  <properties>
+   <property name="Name" value="Yellow"/>
+  </properties>
+ </tile>
+ <tile id="27" type="Wall"/>
+ <tile id="28" type="Wall"/>
+ <tile id="41"/>
+ <tile id="30" type="Wall"/>
+ <tile id="31" type="Wall"/>
+ <tile id="39"/>
+ <tile id="40"/>
+ <tile id="32" type="Wall"/>
+ <tile id="34" type="Enemy">
+  <properties>
+   <property name="Name" value="Yellow"/>
+  </properties>
+ </tile>
+ <tile id="33" type="Wall">
+  <properties>
+   <property name="Name" value="Pink"/>
+  </properties>
+ </tile>
+ <tile id="7" type="Enemy">
+  <properties>
+   <property name="Name" value="Blue"/>
+  </properties>
+ </tile>
+ <tile id="35" type="Berry"/>
+ <tile id="24" type="Door"/>
+ <tile id="37" type="Path"/>
+ <tile id="36" type="WarpA"/>
+ <tile id="42"/>
+ <tile id="26" type="Wall"/>
+ <tile id="3" type="Wall"/>
+</tileset>
